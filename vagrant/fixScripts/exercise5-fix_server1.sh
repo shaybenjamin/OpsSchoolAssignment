bd4e14176384884541abf9420ffd6f773501c779
shayb <shay.benjamin1983@gmail.com>
2020-03-01 11:27:32 +0200

    WIP

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -3,12 +3,19 @@
 sudo apt-get install sshpass && sleep 10
 sudo sed -i 's/#   StrictHostKeyChecking ask/StrictHostKeyChecking no/' /etc/ssh/ssh_config
 sudo sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
-sudo service ssh restart
-sudo chown -R vagrant:vagrant ~/.ssh/ 
-sudo chmod -R go= ~/.ssh/
 
-ssh-keygen -q -t rsa -N '' -f /home/vagrant/.ssh/id_rsa 2>/dev/null <<< y >/dev/null
-sshpass -p vagrant ssh-copy-id -i /home/vagrant/.ssh/id_rsa.pub root@server2
+ssh-keygen -P "" -f /home/vagrant/.ssh/id_rsa
+sudo chmod 777 /home/vagrant/.ssh/id_rsa
+sleep 300&& sshpass -p vagrant ssh-copy-id -i /home/vagrant/.ssh/id_rsa.pub root@server2 &
+
+
+
+# sudo service ssh restart
+# sudo chown -R vagrant:vagrant ~/.ssh/ 
+# sudo chmod -R go= ~/.ssh/
+
+# ssh-keygen -q -t rsa -N '' -f /home/vagrant/.ssh/id_rsa 2>/dev/null <<< y >/dev/null
+# sshpass -p vagrant ssh-copy-id -i /home/vagrant/.ssh/id_rsa.pub root@server2
 
 
 
--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -3,12 +3,18 @@
 sudo apt-get install sshpass & sleep 10
 sudo sed -i 's/#   StrictHostKeyChecking ask/StrictHostKeyChecking no/' /etc/ssh/ssh_config
 sudo sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
-sudo service ssh restart
-sudo chmod -R go= ~/.ssh/
-sudo chown -R vagrant:vagrant ~/.ssh/ 
 
-ssh-keygen -q -t rsa -N '' -f /home/vagrant/.ssh/id_rsa 2>/dev/null <<< y >/dev/null
-sshpass -p vagrant ssh-copy-id -f -i /home/vagrant/.ssh/id_rsa.pub root@server1
+ssh-keygen -P "" -f /home/vagrant/.ssh/id_rsa
+sudo chmod 777 /home/vagrant/.ssh/id_rsa
+sshpass -p vagrant ssh-copy-id -i /home/vagrant/.ssh/id_rsa.pub root@server1
+
+
+# sudo service ssh restart
+# sudo chmod -R go= ~/.ssh/
+# sudo chown -R vagrant:vagrant ~/.ssh/ 
+
+# ssh-keygen -q -t rsa -N '' -f /home/vagrant/.ssh/id_rsa 2>/dev/null <<< y >/dev/null
+# sshpass -p vagrant ssh-copy-id -f -i /home/vagrant/.ssh/id_rsa.pub root@server1
 #ssh-add
 
 
